5137dc4b24a848cc3bbc62fa65e720bf73f3678a
Anil <anil_gurung15@yahoo.com>
2021-04-01 13:01:50 -0700

    created sessions and registrations controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionController < AppicationController
+    def create
+        user = User
+            .find_by(username: params["user"]["username"])
+            .try(:authenticate, params["user"]["password"])
+
+        if user
+            session[:user_id] = user.id
+        end
+    end
+end
